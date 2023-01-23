185b8f766a4c1f5101d427c1b07153a5ae93d600
Ravshan-web <savurboyevr@gmail.com>
2023-01-24 03:38:53 +0500

    3 buttons are ready

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const todolistCollection = document.querySelector('#todolist-collection')
 const todolistInputClearX = document.querySelector('#input-clear') 
 const deleteAlltodolist = document.querySelector('#delete-all-todo');
 
+// Modal Window variables
+const modalWindow = document.querySelector('#modal_window')
+const modalConfirmYes = document.querySelector('#modal_yes')
+const modalConfirmNo = document.querySelector('#modal_no')
+
+const body = document.querySelector("body")
+console.log(body);
+
 todolistInputClearX.addEventListener("click" , ()=>{
     toDoListFormInput.value = "";
 })
@@ -18,7 +26,7 @@ function AddToDo(e){
     const todolistText = document.createElement("p");
     todolistText.innerText = inputValue;
     todolistLi.appendChild(todolistText);
-    todolistCollection.appendChild(todolistLi);
+    todolistCollection.prepend(todolistLi);
     
 
     // li elements setting items
@@ -31,12 +39,20 @@ function AddToDo(e){
     editBtnEl.className = "edit__btn settings";
     editBtnEl.innerHTML = '<i class="bi bi-pencil-square"></i> Edit'
 
+
+    //time btn
     const timeBtnEl = document.createElement("div");
     const time = new Date();
     const now = `${time.getHours().toString().padStart(2, "0")} : ${time. getMinutes().toString().padStart(2, "0")} : ${time.getSeconds().toString().padStart(2, "0")}`
+
+    const current12Hours = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second:'numeric', hour12: true })
+        // console.log(current12Hours);
+
     timeBtnEl.className = "time__btn settings"
     timeBtnEl.innerHTML = `<i class="bi bi-clock"></i> ${now}`
 
+
+    //deletebtn
     const deleteBtnEl = document.createElement("div");
     deleteBtnEl.className = "delete__btn settings"
     deleteBtnEl.innerHTML = '<i class="bi bi-trash3-fill"></i> Remove'
@@ -53,18 +69,72 @@ function AddToDo(e){
     deleteBtnEl.addEventListener("click", ()=>{
         let confirmation = confirm("Do you really want to delete this task?")
         if(confirmation){
-            todolistLi.style.display = "none"
+            todolistLi.remove()
         }
     })  
+
+
+    // COMPLETE ONE OF TASK FROM TOFO LIST
+    doneBtnEl.addEventListener('click', (e)=>{
+            todolistText.classList.toggle("completed") 
+            doneBtnEl.classList.toggle("completed-btn-style")
+    })
+    
+
+    // EDIT BUTTON CODE
+    editBtnEl.addEventListener('click', ()=>{
+        if(todolistText.hasAttribute("contenteditable")){
+            todolistText.removeAttribute("contenteditable")
+            editBtnEl.innerHTML = '<i class="bi bi-pencil-square"></i> Edit'
+            editBtnEl.style.background = "orange"
+        }
+        else{
+            todolistText.setAttribute("contenteditable", true)
+            editBtnEl.innerHTML = '<i class="fas fa-check-double"></i> Save edit'
+            editBtnEl.style.background = "purple"
+        }
+    })
+
+
+    //TIME BUTTON CODE
+    timeBtnEl.addEventListener('click' , ()=>{
+        if(timeBtnEl.innerHTML == `<i class="bi bi-clock"></i> ${now}`){
+            timeBtnEl.innerHTML = `<i class="bi bi-clock"></i> ${current12Hours}`
+        }
+        else{
+            timeBtnEl.innerHTML = `<i class="bi bi-clock"></i> ${now}`
+        }        
+    })
+
 
     toDoListFormInput.value = "";
 }
 
+
+
+
+
+//DELETING ALL TASKS
+
 deleteAlltodolist.addEventListener("click", ()=>{
-    let confirmIt = confirm("Do you really want delete all of tasks???");
-    if(confirmIt){
-        todolistCollection.innerHTML = '';
-        toDoListFormInput.value = "";
-    }
+    modalWindow.style.display = "flex";
+    // let confirmIt = confirm("Do you really want delete all of tasks???");
+    // if(confirmIt){
+    //     todolistCollection.innerHTML = "";
+    //     toDoListFormInput.value = "";
+    // }
 })
+
+    
+
+    
+
+
+    
+    
+
+    
+    
+  
+
 
